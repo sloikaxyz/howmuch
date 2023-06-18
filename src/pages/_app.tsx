aee2c90de0631859909ab553f733ca513ee4982a
Arseniy Ivanov <freeatnet@freeatnet.com>
2023-06-18 12:37:51 +0300

    initialize a T3 app with Tailwind

---
+++ src/pages/_app.tsx
@@ -0,0 +1,8 @@
+import { type AppType } from "next/app";
+import "~/styles/globals.css";
+
+const MyApp: AppType = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default MyApp;
